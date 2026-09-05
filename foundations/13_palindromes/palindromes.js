b1bb6678c890840bdf39239864e1174f17aacb5c
Ap V <21223224+awvivit@users.noreply.github.com>
2026-09-04 23:27:54 -0700

    Created the palindromes javascript that fulfills all of the required test.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (strPal) {
+    //removes the punctuation and spaces and makes lowercase
+    const pArray = Array.from(strPal.toLowerCase().replace(/[^\w\s]|_|\s+/g, "")); 
 
+    
+    return pArray.toReversed().every((val,i) => val === pArray[i]); 
 };
 
 // Do not edit below this line
