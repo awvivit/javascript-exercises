f7fe9e1e1b5cbdb54b96aea8556d53e09defb738
Ap V <21223224+awvivit@users.noreply.github.com>
2026-09-05 00:30:11 -0700

    Finished the fibonacci to spec. I think this could probably be done recursively.  It would be interesting to see if I could do it.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = parseInt(n); 
 
+    if (num < 0) return "OOPS"; 
+    if (num === 0) return 0; 
+    if (num === 1 || num === 2) return 1; 
+
+    let tot = 0; 
+    let p1 = 1; 
+    let p2 = 1; 
+
+    // i =2 is actually the n=3 in this context
+    for (i = 2; i < num; i++) {
+        tot = p1 + p2;
+        p1 = p2; 
+        p2 = tot; 
+    }
+
+    return tot; 
+    
 };
 
 // Do not edit below this line
