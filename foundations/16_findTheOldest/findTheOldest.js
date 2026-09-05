808d2da4a64adba8890f5b6e12b58617c8c2ae92
Ap V <21223224+awvivit@users.noreply.github.com>
2026-09-05 14:30:01 -0700

    See last commit message.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
+//This works but the solution they have is to use a function to figure out the age. 
+// That would probably be better and be eaisier to maintain. 
+
+//I updated my to do so. 
+
+const getAge = function(birth, death) { 
+    const now = new Date().getFullYear(); 
+    
+    return ((death || now) - birth)
+
+
+}
+
 const findTheOldest = function(arrPeople) {
+
+    arrPeople.sort((firstPerson,secondPerson)=>{
+        const firstPersonAge = getAge(firstPerson.yearOfBirth, firstPerson.yearOfDeath);
+        const secondPersonAge = getAge(secondPerson.yearOfBirth, secondPerson.yearOfDeath); 
+
+        return secondPersonAge - firstPersonAge; 
+    });
+
+    return arrPeople[0]; 
     
 };
 
